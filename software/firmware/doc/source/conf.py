cdcccc9bf127b2ebbcf00f63e07f5f5d4f7b82ae
Valentin Roland <valentin@vroland.de>
2020-04-19 20:16:20 +0200

    fix doxygen path

--- software/firmware/doc/source/conf.py
+++ software/firmware/doc/source/conf.py
@@ -68,4 +68,4 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+    subprocess.call('cd ../; doxygen', shell=True)
