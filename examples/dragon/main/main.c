cedd9367186f1f5d1ccfe2fd0e0514915a07e8e0
Valentin Roland <valentin@vroland.de>
2021-01-21 20:04:30 +0100

    make dragon example arduino-compatible

--- examples/dragon/main/main.c
+++ examples/dragon/main/main.c
@@ -20,11 +20,12 @@
 
 uint8_t *img_buf;
 
+#ifndef ARDUINO_ARCH_ESP32
 void delay(uint32_t millis) { vTaskDelay(millis / portTICK_PERIOD_MS); }
-
 uint32_t millis() { return esp_timer_get_time() / 1000; }
+#endif
 
-void loop() {
+void idf_loop() {
   epd_poweron();
   epd_clear();
   volatile uint32_t t1 = millis();
@@ -41,23 +42,42 @@ void loop() {
 }
 
 void epd_task() {
-  epd_init();
 
   while (1) {
-    loop();
+    idf_loop();
   };
 }
 
-void app_main() {
-  // copy the image data to SRAM for faster display time
+void idf_setup() {
+
+  // copy the image data to SRAM for faster display time.
+  // This could also be rendered directly from flash.
   img_buf = (uint8_t *)heap_caps_malloc(EPD_WIDTH * EPD_HEIGHT / 2, MALLOC_CAP_SPIRAM);
   if (img_buf == NULL) {
      ESP_LOGE("epd_task", "Could not allocate framebuffer in PSRAM!");
   }
 
-  memcpy(img_buf, dragon_data, EPD_WIDTH * EPD_HEIGHT / 2);
   heap_caps_print_heap_info(MALLOC_CAP_INTERNAL);
   heap_caps_print_heap_info(MALLOC_CAP_SPIRAM);
 
+
+  memset(img_buf, 0, EPD_WIDTH * EPD_HEIGHT / 2);
+  Rect_t image_size = {
+      .width = dragon_width,
+      .height = dragon_height,
+      .x = 0,
+      .y = 0,
+  };
+  epd_copy_to_framebuffer(image_size, dragon_data, img_buf);
+
+  epd_init();
+}
+
+
+
+#ifndef ARDUINO_ARCH_ESP32
+void app_main() {
+  idf_setup();
   xTaskCreatePinnedToCore(&epd_task, "epd task", 10000, NULL, 2, NULL, 1);
 }
+#endif
