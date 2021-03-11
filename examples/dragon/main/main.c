b54018d3941ea2e9792d9296f669f8cb343f653e
Valentin Roland <valentin@vroland.de>
2021-03-11 15:47:32 +0100

    port dragon example

--- examples/dragon/main/main.c
+++ examples/dragon/main/main.c
@@ -1,63 +1,41 @@
-/* Simple firmware for a ESP32 displaying a static image on an EPaper Screen.
- *
- * Write an image into a header file using a 3...2...1...0 format per pixel,
- * for 4 bits color (16 colors - well, greys.) MSB first.  At 80 MHz, screen
- * clears execute in 1.075 seconds and images are drawn in 1.531 seconds.
- */
+/* Simple firmware for a ESP32 displaying a static image on an EPaper Screen */
 
 #include "esp_heap_caps.h"
-#include "esp_log.h"
-#include "esp_timer.h"
-#include "esp_types.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "sdkconfig.h"
-#include <stdio.h>
-#include <string.h>
 
 #include "epd_driver.h"
+#include "epd_highlevel.h"
 #include "dragon.h"
 
-uint8_t *img_buf;
-
-void delay(uint32_t millis) { vTaskDelay(millis / portTICK_PERIOD_MS); }
-
-uint32_t millis() { return esp_timer_get_time() / 1000; }
+EpdiyHighlevelState hl;
 
 void loop() {
+
+  EpdRect dragon_area = {
+      .x = 0,
+      .y = 0,
+      .width = dragon_width,
+      .height = dragon_height
+  };
+  epd_copy_to_framebuffer(dragon_area, dragon_data, epd_hl_get_framebuffer(&hl));
+
   epd_poweron();
   epd_clear();
-  volatile uint32_t t1 = millis();
-  epd_push_pixels(epd_full_screen(), 20, 0);
-  epd_push_pixels(epd_full_screen(), 20, 0);
-  epd_push_pixels(epd_full_screen(), 20, 0);
-  epd_draw_image(epd_full_screen(), img_buf, WHITE_ON_BLACK);
-  volatile uint32_t t2 = millis();
-  printf("EPD draw took %dms.\n", t2 - t1);
-
+  int temperature = 25;
+  enum EpdDrawError _err = epd_hl_update_screen(&hl, MODE_GC16, temperature);
   epd_poweroff();
 
-  delay(5000);
+  while (1) {
+      vTaskDelay(1000);
+  }
 }
 
-void epd_task() {
-  epd_init();
+void app_main() {
+  epd_init(EPD_OPTIONS_DEFAULT);
+  hl = epd_hl_init(EPD_BUILTIN_WAVEFORM);
 
   while (1) {
     loop();
   };
-}
-
-void app_main() {
-  // copy the image data to SRAM for faster display time
-  img_buf = (uint8_t *)heap_caps_malloc(EPD_WIDTH * EPD_HEIGHT / 2, MALLOC_CAP_SPIRAM);
-  if (img_buf == NULL) {
-     ESP_LOGE("epd_task", "Could not allocate framebuffer in PSRAM!");
-  }
-
-  memcpy(img_buf, dragon_data, EPD_WIDTH * EPD_HEIGHT / 2);
-  heap_caps_print_heap_info(MALLOC_CAP_INTERNAL);
-  heap_caps_print_heap_info(MALLOC_CAP_SPIRAM);
-
-  xTaskCreatePinnedToCore(&epd_task, "epd task", 10000, NULL, 2, NULL, 1);
 }
