79b02e3dc097d00133539e6f60cc9ecb01df1924
Valentin Roland <valentin@vroland.de>
2021-08-25 23:09:05 +0200

    upate docs for v6

--- src/epd_driver/include/epd_board_specific.h
+++ src/epd_driver/include/epd_board_specific.h
@@ -1,31 +1,24 @@
 /**
- * Board-specific functions that are only conditionally defined.
+ * @file "epd_board_specific.h"
+ * @brief Board-specific functions that are only conditionally defined.
  */
 
 #pragma once
 
-#if defined(CONFIG_EPD_BOARD_REVISION_V6)
-#include "pca9555.h"
+#include <stdint.h>
+#include "esp_err.h"
 
 /**
  * Set GPIO direction of the broken-out GPIO extender port.
  */
-static esp_err_t epd_gpio_set_direction(uint8_t direction) {
-    pca9555_set_config(EPDIY_I2C_PORT, direction, 0)
-}
+esp_err_t epd_gpio_set_direction(uint8_t direction);
 
 /**
  * Get the input level of the broken-out GPIO extender port.
  */
-static uint8_t epd_gpio_get_level() {
-    pca9555_read_input(EPDIY_I2C_PORT, 0)
-}
+uint8_t epd_gpio_get_level();
 
 /**
  * Get the input level of the broken-out GPIO extender port.
  */
-static uint8_t epd_gpio_set_value(uint8_t value) {
-    pca9555_set_value(EPDIY_I2C_PORT, value, 0)
-}
-
-#endif
+esp_err_t epd_gpio_set_value(uint8_t value);
