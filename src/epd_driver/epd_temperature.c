ecb5319227330f570bd68db5c261da96a2a63e12
Chris Greening <chris@cmgresearch.com>
2021-09-25 17:09:52 +0100

    Adds a warning when temperature sensor not available

--- src/epd_driver/epd_temperature.c
+++ src/epd_driver/epd_temperature.c
@@ -5,7 +5,9 @@
 
 #ifdef CONFIG_EPD_BOARD_REVISION_LILYGO_T5_47
 void epd_temperature_init() {}
-float epd_ambient_temperature() {
+float epd_ambient_temperature()
+{
+  ESP_LOGW("epd_temperature", "No ambient temperature sensor - returning 21C");
   return 21.0;
 }
 #else
@@ -16,23 +18,31 @@ static esp_adc_cal_characteristics_t adc_chars;
 
 #define NUMBER_OF_SAMPLES 100
 
-void epd_temperature_init() {
+void epd_temperature_init()
+{
   esp_adc_cal_value_t val_type = esp_adc_cal_characterize(
       ADC_UNIT_1, ADC_ATTEN_DB_6, ADC_WIDTH_BIT_12, 1100, &adc_chars);
-  if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+  if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP)
+  {
     ESP_LOGI("epd_temperature", "Characterized using Two Point Value\n");
-  } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+  }
+  else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF)
+  {
     ESP_LOGI("esp_temperature", "Characterized using eFuse Vref\n");
-  } else {
+  }
+  else
+  {
     ESP_LOGI("esp_temperature", "Characterized using Default Vref\n");
   }
   adc1_config_width(ADC_WIDTH_BIT_12);
   adc1_config_channel_atten(channel, ADC_ATTEN_DB_6);
 }
 
-float epd_ambient_temperature() {
+float epd_ambient_temperature()
+{
   uint32_t value = 0;
-  for (int i = 0; i < NUMBER_OF_SAMPLES; i++) {
+  for (int i = 0; i < NUMBER_OF_SAMPLES; i++)
+  {
     value += adc1_get_raw(channel);
   }
   value /= NUMBER_OF_SAMPLES;
@@ -46,7 +56,8 @@ float epd_ambient_temperature() {
 
 void epd_temperature_init() {}
 
-float epd_ambient_temperature() {
+float epd_ambient_temperature()
+{
   return tps_read_thermistor(EPDIY_I2C_PORT);
 }
 
