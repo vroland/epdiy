25a8da291a6630e481a010919fcae478fe98d7d3
martinberlin <martin@luckycloud.de>
2022-04-23 22:24:43 +0200

    Fix Lilygo EPD47 waveform adding ED047TC1 as default #171

--- src/epd_driver/builtin_waveforms.c
+++ src/epd_driver/builtin_waveforms.c
@@ -8,3 +8,6 @@
 #include "waveforms/epdiy_ED097OC4.h"
 #include "waveforms/epdiy_ED097TC2.h"
 #include "waveforms/epdiy_ED133UT2.h"
+// Warning: Alternative Waveform added by Lilygo on 21 Oct 2021
+//          Size: 266 Kb (ED047TC1 is 37 Kb, 7 times smaller)
+//#include "waveforms/epdiy_ED047TC2.h"
