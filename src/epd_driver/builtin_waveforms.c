051477e6336a27204e5a88daa2e425dabb4fdb19
Martin <martin@luckycloud.de>
2022-04-29 10:01:11 +0200

    Update builtin_waveforms.c

--- src/epd_driver/builtin_waveforms.c
+++ src/epd_driver/builtin_waveforms.c
@@ -2,6 +2,8 @@
 
 
 #include "waveforms/epdiy_ED047TC1.h"
+
+// Note: Alternative Waveform added by Lilygo on Oct 2021, size: 266 Kb (ED047TC1 is 37 Kb, 7 times smaller)
 #if defined(CONFIG_EPD_DISPLAY_TYPE_ED047TC2)
   #include "waveforms/epdiy_ED047TC2.h"
 #endif
@@ -11,6 +13,3 @@
 #include "waveforms/epdiy_ED097OC4.h"
 #include "waveforms/epdiy_ED097TC2.h"
 #include "waveforms/epdiy_ED133UT2.h"
-// Warning: Alternative Waveform added by Lilygo on 21 Oct 2021
-//          Size: 266 Kb (ED047TC1 is 37 Kb, 7 times smaller)
-//#include "waveforms/epdiy_ED047TC2.h"
