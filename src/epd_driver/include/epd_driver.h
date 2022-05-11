21e7e6c0f89782ec484ffd56e544214a140eff63
Greg Dickson <greg@spotx.net>
2022-05-11 12:22:51 +0800

    Sphinxify documentation
    Shpinx has different requirements and so now the comment to the function should appear in read thedocs documentation.

--- src/epd_driver/include/epd_driver.h
+++ src/epd_driver/include/epd_driver.h
@@ -208,25 +208,26 @@ void epd_poweron();
 
 #if defined(CONFIG_EPD_BOARD_REVISION_LILYGO_T5_47)
 /** This is a Lilygo47 specific function
- 
-  This is a  work around a hardware issue with the Lilygo47 epd_poweroff() turns off the epaper completely
-  however the hardware of the Lilygo47 is different than the official boards. Which means that on the Lilygo47 this
-  disables power to the touchscreen.
-
-  This is a workaround to allow to disable display power but not the touch screen.
-  On the Lilygo the epd power flag was re-purposed as power enable
-  for everything. This is a hardware thing. 
- \warning This workaround may still leave power on to epd and as such may cause other problems such as grey screen.
-  Please also use epd_poweroff() and epd_deinit() when you sleep the system wake on touch will still work.
-
- Arduino specific code:
- \code{.c}
-  epd_poweroff();
-  epd_deinit();
-  esp_sleep_enable_ext1_wakeup(GPIO_SEL_13, ESP_EXT1_WAKEUP_ANY_HIGH);
-  esp_deep_sleep_start();
-  \endcode
+* 
+* This is a work around a hardware issue with the Lilygo47 epd_poweroff() turns off the epaper completely
+* however the hardware of the Lilygo47 is different than the official boards. Which means that on the Lilygo47 this
+* disables power to the touchscreen.
+*
+* This is a workaround to allow to disable display power but not the touch screen.
+* On the Lilygo the epd power flag was re-purposed as power enable
+* for everything. This is a hardware thing.
+* 
+* Only use this function when ever you have finished drawing to the screen but still need the touch enabled.
+* This workaround may still leave power some other stray power leaks due to the hardware design and as such may cause other problems such as grey screen.
+* Please also use epd_poweroff() and epd_deinit() when you sleep the system wake on touch will still work. 
+*
+* Arduino specific code:
+* epd_poweroff();
+* epd_deinit();
+* esp_sleep_enable_ext1_wakeup(GPIO_SEL_13, ESP_EXT1_WAKEUP_ANY_HIGH);
+* esp_deep_sleep_start();
 */
+
 void epd_powerdown();
 #endif
 
