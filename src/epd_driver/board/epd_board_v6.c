ccbf05e2fafadf677dbb33af3ae034defdf24b53
Micke Prag <micke.prag@gmail.com>
2022-05-22 21:21:09 +0200

    Read vcom voltage from epd_board_vcom_v6()
    This makes vcom voltage available at runtime and not at compile time

--- src/epd_driver/board/epd_board_v6.c
+++ src/epd_driver/board/epd_board_v6.c
@@ -1,4 +1,5 @@
 #include "epd_board.h"
+#include "board/epd_board_v6.h"
 
 #include "esp_log.h"
 #include "../display_ops.h"
@@ -206,13 +207,7 @@ static void epd_board_poweron(epd_ctrl_state_t *state) {
 
   ESP_ERROR_CHECK(tps_write_register(config_reg.port, TPS_REG_ENABLE, 0x3F));
 
-#ifdef CONFIG_EPD_DRIVER_V6_VCOM
-  tps_set_vcom(config_reg.port, CONFIG_EPD_DRIVER_V6_VCOM);
-// Arduino IDE...
-#else
-  extern int epd_driver_v6_vcom;
-  tps_set_vcom(config_reg.port, epd_driver_v6_vcom);
-#endif
+  tps_set_vcom(config_reg.port, epd_board_vcom_v6());
 
   state->ep_sth = true;
   mask = (const epd_ctrl_state_t){
@@ -275,6 +270,16 @@ uint8_t epd_gpio_get_level_v6() {
  */
 esp_err_t epd_gpio_set_value_v6(uint8_t value) {
     return pca9555_set_value(EPDIY_I2C_PORT, value, 0);
+}
+
+uint16_t __attribute__((weak)) epd_board_vcom_v6() {
+#ifdef CONFIG_EPD_DRIVER_V6_VCOM
+  return CONFIG_EPD_DRIVER_V6_VCOM;
+#else
+  // Arduino IDE...
+  extern int epd_driver_v6_vcom;
+  return epd_driver_v6_vcom;
+#endif
 }
 
 const EpdBoardDefinition epd_board_v6 = {
