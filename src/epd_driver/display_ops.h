1f9ae2150adee0d88de40678413ca77291608375
dot4qu <brian.team.jr@gmail.com>
2022-07-06 17:02:42 +0200

    update GPIO usage
    Co-authored-by: jdoubleu <hi@jdoubleu.de>

--- src/epd_driver/display_ops.h
+++ src/epd_driver/display_ops.h
@@ -4,16 +4,32 @@
 #include "driver/gpio.h"
 #include "epd_board.h"
 
+#include "esp_system.h" // for ESP_IDF_VERSION_VAL
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+#include "hal/gpio_ll.h"
+#include "soc/gpio_struct.h"
+#endif
+
 /*
  * Write bits directly using the registers.
  * Won't work for some pins (>= 32).
  */
 inline void fast_gpio_set_hi(gpio_num_t gpio_num) {
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+  gpio_dev_t *device = GPIO_LL_GET_HW(gpio_num);
+  device->out_w1ts = (1 << gpio_num);
+#else
   GPIO.out_w1ts = (1 << gpio_num);
+#endif
 }
 
 inline void fast_gpio_set_lo(gpio_num_t gpio_num) {
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+  gpio_dev_t *device = GPIO_LL_GET_HW(gpio_num);
+  device->out_w1tc = (1 << gpio_num);
+#else
   GPIO.out_w1tc = (1 << gpio_num);
+#endif
 }
 
 void IRAM_ATTR busy_delay(uint32_t cycles);
