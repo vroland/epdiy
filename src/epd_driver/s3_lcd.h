9edadf2346469dc8a808e4fec7483299be800775
Valentin Roland <valentin@vroland.de>
2022-11-07 23:50:43 +0100

    8-bit epd compatible

--- src/epd_driver/s3_lcd.h
+++ src/epd_driver/s3_lcd.h
@@ -1,10 +1,20 @@
 #pragma once
 
-#include "lut.h"
-#include "freertos/FreeRTOS.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-esp_err_t epd_lcd_init();
-void s3_set_frame_prepare_cb(void (*cb)(void));
-void s3_delete_frame_prepare_cb();
-void s3_set_line_source(bool(*line_source)(uint8_t*));
+/// Configuration structure for the LCD-based Epd driver.
+typedef struct {
+    // high time for CKV in 1/10us.
+    size_t pixel_clock; // = 12000000
+    int ckv_high_time; // = 70
+    int line_front_porch; // = 4
+    int le_high_time; // = 4
+    int bus_width; // = 16
+} LcdEpdConfig_t;
+
+void epd_lcd_init(const LcdEpdConfig_t* config);
+void s3_set_frame_done_cb(void (*cb)(void));
+void s3_set_line_source_cb(bool(*line_source)(uint8_t*));
 void s3_start_transmission();
