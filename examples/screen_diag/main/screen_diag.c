12b3311e7dca168d688e1b1183eacfc47023aaba
jdoubleu <hi@jdoubleu.de>
2023-01-15 18:18:00 +0100

    adjust console app for EPD board

--- examples/screen_diag/main/screen_diag.c
+++ examples/screen_diag/main/screen_diag.c
@@ -1,24 +1,15 @@
-/* Basic console example (esp_console_repl API)
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+/* Based on the console example app, which is licensed under CC0.
+ * @link https://github.com/espressif/esp-idf/blob/v4.4.3/examples/system/console/basic/main/console_example_main.c
+ */
 
-   @link https://github.com/espressif/esp-idf/blob/v4.4.3/examples/system/console/basic/main/console_example_main.c
-*/
-
-#include <stdio.h>
 #include <string.h>
-#include "esp_system.h"
-#include "esp_log.h"
-#include "esp_console.h"
-#include "esp_vfs_dev.h"
-#include "esp_vfs_fat.h"
-#include "nvs.h"
-#include "nvs_flash.h"
+#include <esp_log.h>
+#include <esp_console.h>
+#include <esp_vfs_fat.h>
+#include <nvs.h>
+#include <nvs_flash.h>
 
-static const char* TAG = "example";
-#define PROMPT_STR CONFIG_IDF_TARGET
+static const char* TAG = "screen_diag";
 
 static void initialize_filesystem(void)
 {
@@ -48,35 +39,19 @@ static void initialize_nvs(void)
 
 void app_main(void)
 {
-    esp_console_repl_t *repl = NULL;
-    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
-    /* Prompt to be printed before each line.
-     * This can be customized, made dynamic, etc.
-     */
-    repl_config.prompt = PROMPT_STR ">";
-
     initialize_nvs();
     initialize_filesystem();
+
+    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+    repl_config.prompt = "diag>";
     repl_config.history_save_path = "/screen_diag/history.txt";
 
     /* Register commands */
     esp_console_register_help_command();
 
-#if defined(CONFIG_ESP_CONSOLE_UART_DEFAULT) || defined(CONFIG_ESP_CONSOLE_UART_CUSTOM)
+    esp_console_repl_t *repl;
     esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
-
-#elif defined(CONFIG_ESP_CONSOLE_USB_CDC)
-    esp_console_dev_usb_cdc_config_t hw_config = ESP_CONSOLE_DEV_CDC_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_console_new_repl_usb_cdc(&hw_config, &repl_config, &repl));
-
-#elif defined(CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG)
-    esp_console_dev_usb_serial_jtag_config_t hw_config = ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_console_new_repl_usb_serial_jtag(&hw_config, &repl_config, &repl));
-
-#else
-#error Unsupported console type
-#endif
 
     ESP_ERROR_CHECK(esp_console_start_repl(repl));
 }
