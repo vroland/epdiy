da0f202a819752c8db1361ea106a2165ce3facec
Valentin Roland <valentin@vroland.de>
2024-04-30 21:25:20 +0200

    fix render task priority value

--- src/render.c
+++ src/render.c
@@ -324,7 +324,7 @@ void epd_renderer_init(enum EpdInitOptions options) {
         );
         assert(render_context.feed_line_buffers[i] != NULL);
         RTOS_ERROR_CHECK(xTaskCreatePinnedToCore(
-            render_thread, "epd_prep", 1 << 12, (void*)i, configMAX_PRIORITIES,
+            render_thread, "epd_prep", 1 << 12, (void*)i, configMAX_PRIORITIES - 1,
             &render_context.feed_tasks[i], i
         ));
     }
