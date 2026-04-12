d944adbfa4540dbfee92cb4ea21beb06ff173ad3
Valentin Roland <valentin.roland@meltwater.com>
2026-04-12 22:46:42 +0200

    add rmt abstraction layer

---
+++ src/output_common/rmt_compat.c
@@ -0,0 +1,166 @@
+#include "rmt_compat.h"
+
+#include <esp_idf_version.h>
+#include <esp_attr.h>
+#include <esp_intr_alloc.h>
+#include <esp_private/periph_ctrl.h>
+#include <string.h>
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+#include <driver/gpio.h>
+#include <esp_rom_gpio.h>
+#include <hal/rmt_ll.h>
+#include <soc/rmt_reg.h>
+#include <soc/rmt_struct.h>
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(6, 0, 0)
+#include <hal/rmt_periph.h>
+#ifndef SOC_RMT_MEM_WORDS_PER_CHANNEL
+#define SOC_RMT_MEM_WORDS_PER_CHANNEL 48
+#endif
+
+typedef struct {
+    struct {
+        uint32_t data32[SOC_RMT_MEM_WORDS_PER_CHANNEL];
+    } chan[RMT_LL_CHANS_PER_INST];
+} rmt_mem_block_t;
+
+extern rmt_mem_block_t RMTMEM;
+#else
+#include <driver/rmt_types_legacy.h>
+#include <soc/rmt_periph.h>
+typedef rmt_mem_t rmt_mem_block_t;
+extern rmt_mem_block_t RMTMEM;
+#endif
+
+#endif  // ESP_IDF_VERSION >= 5.0.0
+
+static intr_handle_t s_rmt_intr_handle = NULL;
+static void (*s_tx_done_callback)(void*) = NULL;
+static rmt_compat_channel_t s_active_channel = RMT_COMPAT_CHANNEL_MAX;
+
+static void IRAM_ATTR rmt_compat_isr(void* arg) {
+    (void)arg;
+    RMT.int_clr.val = RMT.int_st.val;
+    if (s_tx_done_callback) {
+        s_tx_done_callback(arg);
+    }
+}
+
+void rmt_compat_init(rmt_compat_channel_t channel, gpio_num_t gpio) {
+    rmt_compat_enable_clock(channel);
+    rmt_compat_connect_gpio(channel, gpio);
+    rmt_compat_set_clock_div(channel, 8);
+    rmt_compat_set_mem_blocks(channel, 2);
+
+    rmt_ll_tx_enable_loop(&RMT, channel, true);
+    rmt_ll_tx_enable_carrier_modulation(&RMT, channel, false);
+    rmt_ll_tx_fix_idle_level(&RMT, channel, 0, true);
+    rmt_ll_enable_mem_access_nonfifo(&RMT, true);
+
+    if (s_rmt_intr_handle == NULL) {
+        esp_intr_alloc(
+            ETS_RMT_INTR_SOURCE, ESP_INTR_FLAG_LEVEL3, rmt_compat_isr, NULL, &s_rmt_intr_handle
+        );
+    }
+    RMT.int_ena.val |= (1 << (channel * 3));
+
+    s_active_channel = channel;
+}
+
+void rmt_compat_deinit(rmt_compat_channel_t channel) {
+    (void)channel;
+    RMT.int_ena.val &= ~(1 << (channel * 3));
+    if (s_rmt_intr_handle != NULL) {
+        esp_intr_disable(s_rmt_intr_handle);
+    }
+    rmt_compat_disable_clock(channel);
+}
+
+void rmt_compat_enable_clock(rmt_compat_channel_t channel) {
+    (void)channel;
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(6, 0, 0)
+    PERIPH_RCC_ATOMIC() {
+        rmt_ll_reset_register(0);
+        rmt_ll_enable_bus_clock(0, true);
+    }
+    rmt_ll_enable_group_clock(&RMT, true);
+#else
+    periph_module_reset(PERIPH_RMT_MODULE);
+    periph_module_enable(PERIPH_RMT_MODULE);
+    rmt_ll_enable_periph_clock(&RMT, true);
+#endif
+}
+
+void rmt_compat_disable_clock(rmt_compat_channel_t channel) {
+    (void)channel;
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(6, 0, 0)
+    rmt_ll_enable_group_clock(&RMT, false);
+    PERIPH_RCC_ATOMIC() {
+        rmt_ll_enable_bus_clock(0, false);
+    }
+#else
+    rmt_ll_enable_periph_clock(&RMT, false);
+    periph_module_disable(PERIPH_RMT_MODULE);
+#endif
+}
+
+void rmt_compat_connect_gpio(rmt_compat_channel_t channel, gpio_num_t gpio) {
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(6, 0, 0)
+    esp_rom_gpio_connect_out_signal(
+        gpio, soc_rmt_signals[0].channels[channel].tx_sig, false, 0
+    );
+#else
+    esp_rom_gpio_connect_out_signal(
+        gpio, rmt_periph_signals.groups[0].channels[channel].tx_sig, false, 0
+    );
+#endif
+    gpio_set_direction(gpio, GPIO_MODE_OUTPUT);
+}
+
+void rmt_compat_set_clock_div(rmt_compat_channel_t channel, uint8_t div) {
+    rmt_ll_tx_set_channel_clock_div(&RMT, channel, div);
+}
+
+void rmt_compat_set_mem_blocks(rmt_compat_channel_t channel, uint8_t blocks) {
+    rmt_ll_tx_set_mem_blocks(&RMT, channel, blocks);
+}
+
+void rmt_compat_tx_start(rmt_compat_channel_t channel) {
+    rmt_ll_tx_reset_pointer(&RMT, channel);
+    rmt_ll_tx_start(&RMT, channel);
+}
+
+void rmt_compat_tx_stop(rmt_compat_channel_t channel) {
+    rmt_ll_tx_stop(&RMT, channel);
+}
+
+void rmt_compat_tx_reset_mem(rmt_compat_channel_t channel) {
+    rmt_ll_tx_reset_pointer(&RMT, channel);
+}
+
+void rmt_compat_tx_set_loop(rmt_compat_channel_t channel, bool enable, uint32_t count) {
+    rmt_ll_tx_enable_loop(&RMT, channel, enable);
+    if (enable) {
+        rmt_ll_tx_enable_loop_autostop(&RMT, channel, true);
+        rmt_ll_tx_set_loop_count(&RMT, channel, count);
+    } else {
+        rmt_ll_tx_enable_loop_autostop(&RMT, channel, false);
+    }
+}
+
+void rmt_compat_tx_enable_loop_count(rmt_compat_channel_t channel, bool enable) {
+    rmt_ll_tx_enable_loop_count(&RMT, channel, enable);
+}
+
+void rmt_compat_tx_set_loop_count(rmt_compat_channel_t channel, uint32_t count) {
+    rmt_ll_tx_set_loop_count(&RMT, channel, count);
+}
+
+void* rmt_compat_get_mem_ptr(rmt_compat_channel_t channel) {
+    return (void*)&(RMTMEM.chan[channel].data32[0]);
+}
+
+void rmt_compat_set_tx_done_callback(void (*callback)(void*)) {
+    s_tx_done_callback = callback;
+}
---
+++ src/output_common/rmt_compat.h
@@ -0,0 +1,44 @@
+#ifndef EPDIY_RMT_COMPAT_H
+#define EPDIY_RMT_COMPAT_H
+
+#include <driver/gpio.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    RMT_COMPAT_CHANNEL_0 = 0,
+    RMT_COMPAT_CHANNEL_1 = 1,
+    RMT_COMPAT_CHANNEL_2 = 2,
+    RMT_COMPAT_CHANNEL_3 = 3,
+    RMT_COMPAT_CHANNEL_MAX
+} rmt_compat_channel_t;
+
+void rmt_compat_init(rmt_compat_channel_t channel, gpio_num_t gpio);
+void rmt_compat_deinit(rmt_compat_channel_t channel);
+
+void rmt_compat_enable_clock(rmt_compat_channel_t channel);
+void rmt_compat_disable_clock(rmt_compat_channel_t channel);
+
+void rmt_compat_connect_gpio(rmt_compat_channel_t channel, gpio_num_t gpio);
+void rmt_compat_set_clock_div(rmt_compat_channel_t channel, uint8_t div);
+void rmt_compat_set_mem_blocks(rmt_compat_channel_t channel, uint8_t blocks);
+
+void rmt_compat_tx_start(rmt_compat_channel_t channel);
+void rmt_compat_tx_stop(rmt_compat_channel_t channel);
+void rmt_compat_tx_reset_mem(rmt_compat_channel_t channel);
+void rmt_compat_tx_set_loop(rmt_compat_channel_t channel, bool enable, uint32_t count);
+void rmt_compat_tx_enable_loop_count(rmt_compat_channel_t channel, bool enable);
+void rmt_compat_tx_set_loop_count(rmt_compat_channel_t channel, uint32_t count);
+
+void* rmt_compat_get_mem_ptr(rmt_compat_channel_t channel);
+
+void rmt_compat_set_tx_done_callback(void (*callback)(void*));
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // EPDIY_RMT_COMPAT_H
