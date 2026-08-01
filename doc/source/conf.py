5856cbf3885d0a7dda3cf854bf477b9000fb8f87
Valentin Roland <github@vroland.de>
2026-08-01 15:36:55 +0200

    Bump to 2.1.1 for image releases

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Valentin Roland'
 author = 'Valentin Roland'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '2.1.1'
 
 
 # -- General configuration ---------------------------------------------------
